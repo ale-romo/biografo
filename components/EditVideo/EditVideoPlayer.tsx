9abf7eb65e47d94f7b5f77ec73cad700aa663061
Luis Edgar Llanos <edgardl@exatec.tec.mx>
2023-03-31 10:43:52 -0600

    test

---
+++ components/EditVideo/EditVideoPlayer.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import styles from './EditVideoPlayer.module.css';
+
+
+import { useState, useRef, useCallback, useEffect, SetStateAction } from 'react';
+
+
+
+interface Props {
+    source?: string,
+    timeStart: number,
+    timeEnd: number,
+
+
+}
+
+const EditVideoPlayer =({ source, timeStart, timeEnd }:Props) => {
+    const videoRef = useRef(null)
+    const [isFinished, setIsFinished] = useState(false);
+
+    useEffect(() => {
+      (videoRef.current as any).currentTime = timeStart;
+      console.log('Donasdfasdfasdfe!')
+    }, [])
+
+    const handleTimeUpdate = useCallback( () => {
+      if((videoRef.current as any).currentTime > timeEnd){
+        if(videoRef.current){
+          (videoRef.current as any).pause()
+        }
+        setIsFinished(true);
+      }
+    },[isFinished, setIsFinished]);
+
+    let video = <video autoPlay={false} controls={false} muted={false} onTimeUpdate={handleTimeUpdate} ref={videoRef} src={source} className={styles.editVideoPlayer}>
+    </video>
+
+    return video
+  }
+  
+  export default EditVideoPlayer;
+  
--- pages/videoTest.tsx
+++ pages/videoTest.tsx
@@ -2,37 +2,37 @@ import probe from 'probe-image-size';
 
 import VideoRecorder from 'components/Webcam/Webcam'
 import { LargeText } from 'components/TextFormats/TextFormats';
+import EditVideoPlayer from 'components/EditVideo/EditVideoPlayer';
 
-export const getStaticProps = async ({params}: any) => {
-  const itemRes: any = await fetch(`https://biografoimaginario.com:8888/getObjects?ID=${params.pid}`);
-  const itemData = await itemRes.json();
-  const item = itemData[0];
-  const videoRes: any =  await fetch(`https://biografoimaginario.com:8888/getVideos?ID=${1}`)
-  const video = await videoRes.json();
+// export const getStaticProps = async ({params}: any) => {
+//   const itemRes: any = await fetch(`https://biografoimaginario.com:8888/getObjects?ID=${params.pid}`);
+//   const itemData = await itemRes.json();
+//   const item = itemData[0];
+//   const videoRes: any =  await fetch(`https://biografoimaginario.com:8888/getVideos?ID=${1}`)
+//   const video = await videoRes.json();
 
-  const images = JSON.parse(item.images);
+//   const images = JSON.parse(item.images);
 
-  item.imagesWithSizes = await Promise.all(
-    images.map(async (image: string) => {
-      return {
-        url: `https://biografoimaginario.com:8888/${image}`,
-        size: await probe(`https://biografoimaginario.com:8888/${image}`)
-      }
-    })
-  );
+//   item.imagesWithSizes = await Promise.all(
+//     images.map(async (image: string) => {
+//       return {
+//         url: `https://biografoimaginario.com:8888/${image}`,
+//         size: await probe(`https://biografoimaginario.com:8888/${image}`)
+//       }
+//     })
+//   );
 
-  return {
-    props: {
-      item: item,
-      video: video,
-    }
-  }
-}
+//   return {
+//     props: {
+//       item: item,
+//       video: video,
+//     }
+//   }
+// }
 
-const HomePage = ({ items }: any) => {
+const HomePage = () => {
   return <>
-    <LargeText>Mandando oferta para comprar {data.title}</LargeText>
-    <VideoRecorder objectid={'1'} uid={'1'}></VideoRecorder>
+    <EditVideoPlayer source='https://biografoimaginario.com:8888/uploads/9/3/5/f/2f40-a426-4444-a079-509d36843dc3.mp4' timeEnd={2.2} timeStart={1}></EditVideoPlayer>
   </>
 }
 
